e27fd24757acc61a5c7593843e559ba8fb4db92d
The Lazy One <achuakh@gmail.com>
2025-07-14 06:43:29 +0000

    more functions

--- app.py
+++ app.py
@@ -7,19 +7,24 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-@app.route("/prediction",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    # db
+    return(render_template("main.html"))
 
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
-
     # load model
     model = joblib.load("dbs.jl")
-
     # make prediction
     pred = model.predict([[q]])
-
     return(render_template("prediction.html",r=pred))
 
-if __name__ == "__main__": # for local testing
-    app.run() 
-
+if __name__ == "__main__":
+    app.run()
