52880cb0ff905d697507ae0dfa90b4b60fe9ac71
The Lazy One <achuakh@gmail.com>
2025-07-14 03:55:27 +0000

    use previousely saved model

--- app.py
+++ app.py
@@ -8,9 +8,19 @@ def index():
     return(render_template("index.html"))
 
 @app.route("/prediction",methods=["GET","POST"])
+
 def prediction():
     q = float(request.form.get("q"))
-    return(render_template("prediction.html",r=(-50.6*q)+90.2))
+
+    # load model
+    model = joblib.load("dbs.jl")
+
+    # make prediction
+    pred = model.predict([[q]])
+
+    return(render_template("prediction.html",r=pred)
+
+    # return(render_template("prediction.html",r=(-50.6*q)+90.2))
 
 if __name__ == "__main__": # for local testing
     app.run() 
