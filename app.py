d34b3bfacf808e29f85c03192fc98353d6c8ff39
achuakh <achuakh@gmail.com>
2025-07-16 13:51:52 +0800

    more telegram functions

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import requests
 
 app = Flask(__name__)
 
+# for local test
+
+#################
+
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 @app.route("/",methods=["GET","POST"])
@@ -95,6 +99,23 @@ def telegram():
         status = "Failed to start the telegram bot. Please check the logs."
 
     return(render_template("telegram.html"))
+
+@app.route("/telegram_stop",methods=["GET","POST"])
+def stop_telegram():
+
+    domain_url = 'https://dsat-ft1-ipop.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    webhook_response = requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot is stopped. "
+    else:
+        status = "Failed to stop the telegram bot. Please check the logs."
+    
+    return(render_template("telegram.html", status=status))
 
 @app.route("/webhook",methods=["GET","POST"])
 def webhook():
